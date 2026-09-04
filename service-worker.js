1f0a3b1ee7124c3af810cca4f3c8460493dfc470
san31644-max <ofm43007@gmail.com>
2026-09-05 04:07:42 +0530

    Lower landing page logo position

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='k-education-app-v9';
+const CACHE='k-education-app-v10';
 const ROOT=new URL('./',self.registration.scope);
 const PUBLIC=['','index.php','home.php','login.php','register.php','manifest.json','logo/k-transparent.png','logo/k-landing-transparent.png','logo/k.png','assets/js/app.js'];
 const urls=PUBLIC.map(path=>new URL(path,ROOT).href);
